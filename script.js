fa67f2d72f631b31d34303a2524d0cacaf69e56f
AXEL JANIAK <axeljpro@gmail.com>
2024-03-14 14:48:05 +0100

    add change to home page, change font and logo align

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const reponse = await fetch("data.json");
 const database = await reponse.json();
 
 console.log(database);
+
+new Carousel(document.querySelector('.carousel') {
+    slidesToScroll: 1,
+    slidesVisbles: 3
+})
