4756cde13e62b347e1f7b3cfabc303905a681d85
Dino Saure <lejeune.chris@protonmail.com>
2024-03-15 17:26:33 +0100

    Insertion fichier search.js

---
+++ search.js
@@ -0,0 +1,47 @@
+const reponse = await fetch("data.json");
+const dataBase = await reponse.json();
+// import filename from "path" assert{type:'json'}
+
+/*a modifier
+import { container, createCard } from "./createCard";
+import houseToRent from "./houseToRent"; */
+
+// creation const recherche nom 
+// const name = dataBase[].name	
+
+// je boucle sur mon tableau de noms
+for (let i = 0; i < dataBase.length; i++) {
+  const currentPerson = dataBase[i].name;
+//    createCard(currentPerson);
+
+  console.log(currentPerson)
+}
+
+// j'écoute l'event
+
+const searchInput = document.getElementById("search-input");
+
+searchInput.addEventListener("input", (event) => {
+  const searchValue = event.target.value;
+  container.innerHTML = "";
+
+  currentPerson.filter((person) => {
+    if (person.name.toLowerCase().includes(searchValue.toLowerCase())) {
+      createCard(person);
+    }
+  });
+});
+
+// si une lettre est trouvée, on affiche les cartes 
+
+const showAvailable = document.getElementById("available");
+
+showAvailable.addEventListener("search", () => {
+  container.innerHTML = "";
+  const isChecked = showAvailable.checked;
+  if (isChecked) {
+    currentPerson.filter((person) => person.available && createCard(person));
+  } else {
+    currentPerson.map((person) => createCard(person));
+  }
+});
