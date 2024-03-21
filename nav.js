22e0360b78a50a6246dbf1c0623849175c89b392
AXEL JANIAK <axeljpro@gmail.com>
2024-03-21 14:39:21 +0100

    "add tout plein de truc, ca marche !"

--- nav.js
+++ nav.js
@@ -1,7 +1,21 @@
-// début page index //
-// header navbar//
-const returnButtonMobile = querySelector(".backButtonMobile");
-const returnButtonDesktop = querySelector(".backButtonDesktop");
-const logoTeamHead = querySelector(".logoWCS");
+const buttonMobile = document.getElementById("backButtonMobile");
+const buttonDesktop = querySelector(".backButtonDesktop");
+const nav = document.querySelector("nav");
+const logoTeam = document.querySelector(".logoTeam");
+const slideData = document.querySelector("#slidesDate");
+const slideJS = document.querySelector("#slideJS");
+
+slideData.addEventListener("click", () => {
+    console.log("hello");
+    buttonMobile.style.display = "flex";
+    nav.style.justifyContent = "space-between";
+    logoTeam.style.display = "flex";
+})
+slideJS.addEventListener("click", () => {
+    console.log("hello");
+    buttonMobile.style.display = "flex";
+    nav.style.justifyContent = "space-between";
+    logoTeam.style.display = "flex";
+})
 
 
